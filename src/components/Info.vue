95b310554dc6fede558e2489742ffb0956dab065
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-04 15:15:06 -0300

    Atributos Dinâmicos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -9,17 +9,24 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
+        <p>Para acessar meu portifólio <a v-bind:href='meu_link' target="_blank">  basta clicar aqui</a></p>
+        <Picture/>
     </div>
 </template>
 
 <script>
+import Picture from './Picture.vue'
     export default {
         name:'InfoF',
-        data(){
+        components: {
+            Picture
+        },
+        data() {
             return{
                 esta_trabalhando: false,
                 mostrar_email: true,
-                email: 'nicolefernandesfm@gmail.com'
+                email: 'nicolefernandesfm@gmail.com',
+                meu_link: 'https://github.com/nicole-fer/Curso-Vue'
             }
         }
     }
---
+++ src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="avatar" :alt="descricao">
+</template>
+
+<script>
+    export default {
+        name: 'PicTure',
+        data() {
+            return{
+                avatar: "/img/introduction-to-android-1.png",
+                descricao: 'Avatar Android andando de Skate'
+            }
+        }
+    }
+</script>
