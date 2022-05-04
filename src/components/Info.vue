18cfee23eff3271abd1e048e9f6631c0add60cb8
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-04 14:46:11 -0300

    v-if, v-else, v-show

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <p> Estou trabalhando no momento</p>
+        <p v-if="esta_trabalhando"> Estou trabalhando no momento</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecnologias: </p>
         <ul>
             <li>JavaScript</li>
             <li>PHP</li>
             <li>Python</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name:'InfoF'
+        name:'InfoF',
+        data(){
+            return{
+                esta_trabalhando: false,
+                mostrar_email: true,
+                email: 'nicolefernandesfm@gmail.com'
+            }
+        }
     }
 </script>
