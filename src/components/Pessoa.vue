f0871e3e5f8e3bcb1f887c6929305738b4b7c99e
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-04 13:52:06 -0300

    Hierarquia de componentes

---
+++ src/components/Pessoa.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Esta é a descrição da pessoa: {{nome}} </h2>
+    <p> Estou trabalhando no momento</p>
+    <p>Utilizo as seguintes tecnologias: </p>
+    <ul>
+        <li></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'PessoaP',
+        data(){
+            return{
+                nome: 'Zezim'
+            }
+        }
+    }
+</script>
