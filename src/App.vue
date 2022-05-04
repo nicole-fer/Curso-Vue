c6a0dbd1bc0a3c716bad31ebc5cd8897d9ce605b
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-04 15:59:13 -0300

    Implementando Css em listas(header)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header/>
     <PrimeiroComponente />
     <p class="paragrafo-pai"> O dado de app é: {{testando}}</p>
     <LifeCycle/>
@@ -9,6 +10,7 @@
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue'
 import Pessoa from './components/Pessoa.vue'
@@ -16,6 +18,7 @@ import Pessoa from './components/Pessoa.vue'
 export default{
   name:'App',
   components: {
+    Header,
     PrimeiroComponente,
     LifeCycle,
     Pessoa,
@@ -31,7 +34,7 @@ export default{
 <style>
 
   body {
-    background-color: rgb(75, 75, 75);
+    background-color: rgb(15, 15, 15);
     color: bisque;
   }
 
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul>
+        <li>
+            <a href="#"> Home</a>
+        </li>
+
+        <li>
+            <a href="#"> Projetos</a>
+        </li>
+
+        <li>
+            <a href="#"> Contatos</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name:'HeadeR'
+    }
+</script>
+
+
+<style scoped>
+    ul{
+        list-style: none;
+        display: flex;
+        background: white;
+    }
+    li {
+        margin-right: 10px;
+        font-size: 22px;
+    }
+    a {
+        color: rgb(12, 97, 171);
+        text-decoration: none;
+        transition: .5s;
+    }
+    a:hover{
+        color: turquoise;
+    }
+</style>
