96aedbb1b93b09d0e627003ff0a99546bd7a2e23
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-04 16:19:30 -0300

    Renderização de Listas (v-for)

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,11 +2,13 @@
     <div>
         <p v-if="esta_trabalhando"> Estou trabalhando no momento</p>
         <p v-else>Estou em busca de novas oportunidades</p>
-        <p>Utilizo as seguintes tecnologias: </p>
+        <p>Utilizo as seguintes tecnologias para back-end: </p>
+        <ul >
+            <li v-for="(technology, index) in backend_technology" v-bind:key="index">{{technology}}</li>
+        </ul>
+        <p>Utilizao as seguintes tecnologias para front-end</p>
         <ul>
-            <li>JavaScript</li>
-            <li>PHP</li>
-            <li>Python</li>
+            <li v-for="technology in frontend_technology" :key="technology.id">{{ technology.language }}</li>
         </ul>
         <div>
             <button @click="showEmail">{{textoBotao}}</button>
@@ -30,7 +32,13 @@ import Picture from './Picture.vue'
                 mostrar_email: false,
                 email: 'nicolefernandesfm@gmail.com',
                 meu_link: 'https://github.com/nicole-fer/Curso-Vue',
-                textoBotao: 'Mostrar E-Mail'
+                textoBotao: 'Mostrar E-Mail',
+                backend_technology: ['JavaScript','PHP','Python'],
+                frontend_technology: [
+                    {id: 1 , language: 'HTML'},
+                    {id: 2, language: 'CSS'},
+                    {id: 3, language: 'Vue'}
+                ]
             }
         },
         methods: {
