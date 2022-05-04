0d228a35815b4218ee2d77ae94fb79e7ba68ea70
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-04 17:02:39 -0300

    Multiplos Eventos

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1,$event), segundo(txt2,$event), terceiro()" >Ativar Multiplos Eventos</button>
+        <p>{{multiplos_eventos}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MultiplosEventos',
+        txt1: 'Primeiro Evento',
+        txt2: 'Segundo Evento',
+        data() {
+            return {
+                multiplos_eventos: ''
+            }
+        },
+        // ativa dois metodos com apenas um evento, no primeiro, ao clicar no botão, aparece a frase "Primeiro Evento Ativado" e no segundo substitui a primeira fase pela segunda após meio segundo
+        methods: {
+            primeiro(txt) {
+                this.multiplos_eventos= txt
+            },
+            segundo(txt) {
+                setTimeout(() => this.multiplos_eventos = txt,500)
+            },
+            terceiro() {
+                setTimeout(() => this.multiplos_eventos = " ",1500)
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        margin: 100px;
+    }
+</style>
+
+
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,12 +2,17 @@
     <div>
         <h1>Olá Vue</h1>
         <p> Meu nome é {{nome}}, tenho {{age}} anos, sou {{profissao}}</p>
+        <MultiplosEventos/>
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
     export default {
         name: 'PrimeiroComponente',
+        components : {
+            MultiplosEventos
+        },
         data(){
             return {
                 nome: 'Nicole',
