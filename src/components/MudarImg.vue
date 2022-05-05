b1a6f9968aab1c236f55909cdc8e589e3c79499d
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-05 14:43:48 -0300

    Emit

---
+++ src/components/MudarImg.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button @click="$emit('mudarImg')">Trocar Imagem de Perfil</button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'MudarImg',
+        emits: ['mudarImg']
+    }
+</script>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,20 +1,32 @@
 <template>
     <img :src="avatar" :alt="descricao">
-    <Reutilizacao/>
+    <MudarImg @mudar-img="trocarImg"/>
 </template>
 
 <script>
-import Reutilizacao from './Reutilizacao'
+//import Reutilizacao from './Reutilizacao'
+
+import MudarImg from './MudarImg.vue'
 
     export default {
         name: 'PicTure',
         components : {
-            Reutilizacao
+            MudarImg
+            //Reutilizacao
         },
         data() {
             return{
                 avatar: "/img/introduction-to-android-1.png",
-                descricao: 'Avatar Android andando de Skate'
+                descricao: 'Avatar Android andando de Skate',
+            }
+        },
+        methods: {
+            trocarImg(){
+                if (this.avatar == "/img/introduction-to-android-1.png"){
+                    this.avatar = "/img/fto.png"
+                } else {
+                    this.avatar = "/img/introduction-to-android-1.png"
+                }
             }
         }
     }
