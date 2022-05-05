669375034084eb004df5feae51d967097c6fc236
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-05 16:28:35 -0300

    Atualizações

---
+++ project_make_your_burger/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
