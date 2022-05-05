816eaebe30765ecb6bc367c3559fcd66efcc9d38
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-05 16:43:43 -0300

    Inicializando Footer, Nav e Pedidos

--- project_make_your_burger/src/App.vue
+++ project_make_your_burger/src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar/>
   <router-view/>
+  <Footer/>
 </template>
 
-<style>
+<script>
+import Navbar from "./components/Navbar.vue"
+import Footer from "./components/Footer.vue"
 
+  export default {
+    components: {
+      Navbar,
+      Footer
+    }
+  }
+</script>
+
+
+<style>
+  
 </style>
--- project_make_your_burger/src/components/Navbar.vue
+++ project_make_your_burger/src/components/Navbar.vue
@@ -1,12 +1,15 @@
 <template>
     <div id="nav">
+        <router-link to="/"><img src="" id="logo"></router-link> |
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
     </div>
 </template>
 
 <script>
-
+    export default {
+        name: 'NavbarComponent'
+    }
 </script>
 
 <style scoped>
