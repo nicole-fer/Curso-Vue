6c87911d65ccdf9a8a6d076291c8898f628f540f
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-06 14:32:30 -0300

    Estilizando Formulario de Cadastro

--- project_make_your_burger/src/components/BurgerForm.vue
+++ project_make_your_burger/src/components/BurgerForm.vue
@@ -16,7 +16,7 @@
                     </select>
                 </div>
 
-                <div class="input-container">
+                <div id="opcionais-container" class="input-container">
                     <label for="carne">Escolha a Carne do seu Burger: </label>
                     <select name="carne" id="carne" v-model="pao">
                         <option value="">Selecione o tipo de Carne</option>
@@ -26,7 +26,7 @@
 
                 
                 <div class="input-container">
-                    <label for="carne">Selecione os opcionais: </label>
+                    <label id="opcionais-title" for="opcionais">Selecione os opcionais: </label>
                         <div class="checkbox-container">
                             <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
                             <span>Salame</span>
@@ -48,5 +48,72 @@
 </script>
 
 <style scoped>
+    #burger-form{
+        max-width: 400px;
+        margin: 0 auto;
+    }
 
+    .input-container{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label{
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #FCBA03;
+    }
+
+    input, select{
+        padding: 5px 10px;
+        width: 300px;
+    }
+
+    #opcionais-container{
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-title{
+        width: 100%;
+    }
+
+    .checkbox-container{
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span, 
+    .checkbox-container input {
+        width: auto;
+    }
+
+    .checkbox-container span{
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn{
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 5 auto;
+        cursor: pointer;
+        transition: .5s;
+
+    }
+    
+    .submit-btn:hover{
+        background-color: transparent;
+        color: #222;
+    }
+    
 </style>
