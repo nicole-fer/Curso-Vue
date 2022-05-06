a40a4730f9c85ba93b1e49c4e6479f1214a3e943
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-06 13:56:06 -0300

    Criando banner de aplicação

--- project_make_your_burger/src/views/HomeView.vue
+++ project_make_your_burger/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
+  <Banner/>
   <div class="main-conteiner">
-    <h1>HOME</h1>
+    <h1>Monte o Seu Burger:</h1>
+    <p>formulario</p>
+
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
 
+  export default {
+    name: 'HommeCompontent',
+    components: {
+      Banner
+    }
+  }
 </script>
