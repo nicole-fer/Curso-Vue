a628812fbf2da7cd5c4f8a10ee4294d34bb7706f
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-09 13:49:25 -0300

    Resgatando Pedidos no Banco #02

--- project_make_your_burger/src/components/Dashboards.vue
+++ project_make_your_burger/src/components/Dashboards.vue
@@ -11,43 +11,24 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Nome do Cliente</div>
-                <div>Pao 1</div>
-                <div>Carne 2</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{burger.id}}</div>
+                <div>{{burger.nome}}</div>
+                <div>{{burger.pao}}</div>
+                <div>{{burger.carne}}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">{{opcional}}</li>
                     </ul>
                 
             </div>
             <div>
-                <select name="status" class="status">
-                <option value="">Selecione os dados do Hamburger</option>
+                <select name="status" class="status"  >
+                <option value="" v-for="status in status" :key="status.id" >
+                    {{status}}
+                    </option>
                 </select>
-                <button class="delete-bnt">Cancelar</button>
-                </div>
-        </div>
-
-                    <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Nome do Cliente</div>
-                <div>Pao 1</div>
-                <div>Carne 2</div>
-                <div>
-                    <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
-                    </ul>
-                
-            </div>
-            <div>
-                <select name="status" class="status">
-                <option value="">Selecione os dados do Hamburger</option>
-                </select>
-                <button class="delete-bnt">Cancelar</button>
+                <button class="delete-btn">Cancelar</button>
                 </div>
         </div>
     </div>
@@ -56,7 +37,28 @@
 
 <script>
     export default {
-        name: "DashboardsComponent"
+        name: "DashboardsComponent",
+        data(){
+            return{
+                burgers:null,
+                burgers_id:null,
+                status:[]
+            }
+        },
+        methods: {
+            async getPedidos(){
+                const req = await fetch("http://localhost:3000/burgers")
+
+                const data = await req.json()
+                this.burgers = data
+                this.status = data.status
+
+                //resgatar os status
+            }
+        },
+        mounted() {
+            this.getPedidos()
+        }
     }
 </script>
 
@@ -64,6 +66,7 @@
     #burger-table{
         max-width: 1200px;
         margin: 0 auto;
+
     }
 
     #burger-table-heading,
@@ -71,6 +74,7 @@
     .burger-table-row{
         display: flex;
         flex-wrap: wrap;
+
     }
 
     #burger-table-heading {
@@ -80,7 +84,7 @@
     }
 
     #burger-table-heading div,
-    .burger-table-row {
+    .burger-table-row div {
         width: 19%;
     }
 
@@ -97,7 +101,25 @@
     }
 
     select {
-        
+        padding: 12px 6px;
+        margin-right: 12px;
+    }
+
+    .delete-btn{
+        background-color:#222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .delete-btn:hover{
+        background-color: transparent;
+        color: #222;
     }
 
 </style>
